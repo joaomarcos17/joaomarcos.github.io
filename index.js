8a3d4dcda0683b64c8a1ecb70153d91354c34752
Joao Marcos <79666534+joaomarcos17@users.noreply.github.com>
2021-05-10 20:35:25 +0100

    my files

---
+++ index.js
@@ -0,0 +1,16 @@
+// my porfolio page 
+// mobile menu !!
+const burgerIcon = document.querySelector('#burger');
+
+const navbarMenu = document.querySelector('#navlinks');
+
+burgerIcon.addEventListener('click', () => {
+
+    navbarMenu.classList.toggle('is-active');
+
+
+});
+
+// let user = window.prompt("What is your name?");
+
+// alert("welcome on my site " + user);
