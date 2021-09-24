6377d9cc7e82cff7a2d9914de56d42aa6f143eff
Joao Marcos <79666534+joaomarcos17@users.noreply.github.com>
2021-09-24 12:05:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 // my porfolio page 
 // mobile menu !!
-const burgerIcon = document.querySelector('#burger');
+document.addEventListener('DOMContentLoaded', () => {
 
-const navbarMenu = document.querySelector('#navlinks');
-
-burgerIcon.addEventListener('click', () => {
-
-    navbarMenu.classList.toggle('is-active');
-
-
-});
-
-// let user = window.prompt("What is your name?");
-
-// alert("welcome on my site " + user);
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+  
+      // Add a click event on each of them
+      $navbarBurgers.forEach( el => {
+        el.addEventListener('click', () => {
+  
+          // Get the target from the "data-target" attribute
+          const target = el.dataset.target;
+          const $target = document.getElementById(target);
+  
+          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+          el.classList.toggle('is-active');
+          $target.classList.toggle('is-active');
+  
+        });
+      });
+    }
+  
+  });
